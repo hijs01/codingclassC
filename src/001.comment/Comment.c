07d53e2c2d7b9880bc6f5650ba89c44a33c5560f
이정수 <hijs0146@gmail.com>
2021-04-24 07:45:48 +0000

    주석 실습 추가

---
+++ src/001.comment/Comment.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+	
+{
+	/*
+	가
+	나
+	다
+	라
+	마
+	바
+	*/
+	printf("Hi, Computer \n");
+	printf("Hi, Human \n");
+	// contro / = 드래그 된 코딩들 주석 처리
+	printf("Hello World!\n");
+	
+	return 0; 
+}
